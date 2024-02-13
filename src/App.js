9058a461e3e819162ae25ff52f26293ce4e2f55c
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-13 14:04:46 +0100

    Updated css and drafted footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Header from "./Header";
 import CurrentCityAndWeather from "./CurrentCityAndWeather";
 import CurrentWeatherDetails from "./CurrentWeatherDetails";
 import Forecast from "./Forecast";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
-      <CurrentCityAndWeather />
-      <CurrentWeatherDetails />
-      <Forecast />
+      <div className="container">
+        <Header />
+        <CurrentCityAndWeather />
+        <CurrentWeatherDetails />
+        <Forecast />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      Coded 👩‍💻 by{" "}
+      <a
+        href="https://github.com/j4mieLeigh"
+        target="_blank"
+      >
+        Jamie Leigh
+      </a>{" "}
+      | Open-sourced 👋 on{" "}
+      <a
+        href="https://github.com/j4mieLeigh/weather-app-react-draft"
+        target="_blank"
+      >
+        Github
+      </a>{" "}
+      | Hosted 🥳 by{" "}
+      <a
+        href="superb-marshmallow-3d25b4.netlify.app"
+        target="_blank"
+      >
+        Netlify
+      </a>
+    </footer>
+  );
+}
